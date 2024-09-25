89c5ac8e68a11b3fb9df2f84ab7b4f8518b3f3e4
lai2-24 <lai2-24@itismeucci.com>
2024-09-25 12:08:38 +0200

    quarto commit

---
+++ src/main/java/com/example/MyThread.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MyThread implements Runnable{
+    int l;
+    public MyThread(int p1){
+        l=p1;
+    }
+    public void run(){
+        for(int i =0;i<100;i++){
+        System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+    }
+
